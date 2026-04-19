2665f8d78a9b00e4b0fce325b250a2d950dff577
Kostas Koukakis <kkoukakis@tudelft.nl>
2026-04-19 14:48:14 +0200

    added classes format in the rest

---
+++ Biosignals/Biosignals.py
@@ -0,0 +1,11 @@
+
+
+class Biosignals:
+
+    def __init__(self):
+        print("constructor done")
+
+
+if __name__ == "main":
+    biosignal = Biosignals()
+    
---
+++ Mechatronics/Mechatronics.py
@@ -0,0 +1,8 @@
+class Mechatronics:
+
+    def __init__(self):
+        print("constructor done")
+
+
+if __name__ == "main":
+    mechatronic = Mechatronics()
